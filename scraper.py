e8912e7a6a665a495dddd0001a48e7f68506e354
Davis Haupt <dbh937@gmail.com>
2015-11-22 09:50:34 -0500

    NYTimes works

--- scraper.py
+++ scraper.py
@@ -34,20 +34,19 @@ class Newspaper:
     raise NotImplementedError('Abstract Method')
 
   def get_headline_text(self):
-    """ Get the actual text of the headline. """
-    raise NotImplementedError('Abstract Method')
+    return self.get_headline().a.string
 
   def get_headline_url(self):
-    """ Get the URL of the full headline story. """
-    raise NotImplementedError('Abstract Method')
-
+    return self.get_headline().a.get('href')
+  
   def get_headline_soup(self):
-    """ Get the BS object for the headline page. """
-    raise NotImplementedError('Abstract Method')
+    page_url = self.get_headline_url()
+    html = requests.get(page_url).text
+    return BeautifulSoup(html, 'html.parser')
 
-  def get_image_url(self, page_url):
-    """ Retrieve the URL for the headline image. """
-    raise NotImplementedError('Abstract Method')
+  def get_image_url(self):
+    s = self.get_headline_soup()
+    return s.img.get('src')
 
 class NYTimes(Newspaper):
   """
@@ -57,7 +56,7 @@ class NYTimes(Newspaper):
   and finds the url of the image.
   """
   def __init__(self):
-    super(NYTimes, self).__init__('http://nytimes.com')
+    Newspaper.__init__(self, 'http://nytimes.com')
 
   def get_articles(self):
     return self.soup.find_all('article')
@@ -65,23 +64,19 @@ class NYTimes(Newspaper):
   def get_headline(self):
     return self.get_articles()[0];
 
-  def get_headline_text(self):
-    return self.get_headline().a.string
+class WSJ(Newspaper):
+  def __init__(self):
+    Newspaper.__init__(self, 'http://www.wsj.com/')
 
-  def get_headline_url(self):
-    return self.get_headline().a.get('href')
-  
-  def get_headline_soup(self):
-    page_url = get_headline_url()
-    html = requests.get(page_url).text
-    return BeautifulSoup(html, 'html.parser')
+  def get_articles(self):
+    return self.soup.find_all('h3', 'wsj-headline')
 
-  def get_image_url(self):
-    s = get_headline_soup()
-    return s.img.get('src')
+  def get_headline(self):
+    return self.get_articles()[0]
 
-
-
+if __name__ == '__main__':
+  i = WSJ()
+  print i.get_image_url()
 
 
 
